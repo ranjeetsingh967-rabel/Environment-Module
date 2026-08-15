2108d524b4b6f35f7844e6e2d9fe924214a4ab63
Ranjeetsingh967-rabel <ranjeetsingh967@gmail.com>
2026-08-15 18:44:25 +0530

    code modify done

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name = "vpn-rg"
-    storage_account_name = "storagevpn123"
+    storage_account_name = "agevpn123"
     container_name       = "vpncontainer"
     key                  = "dev_terraform.tfstate"
   }
